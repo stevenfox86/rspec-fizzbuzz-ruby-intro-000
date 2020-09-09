f3ab5155d7dca746aa51006d7ed530a10d54f87b
steven fox <steven.fox86@gmail.com>
2020-09-09 17:06:48 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,10 +3,7 @@
 
 
 def fizzbuzz(int)
-  if int % 15 == 0 # if the number int is divisible by 3
-    "FizzBuzz"
-
-  elsif int % 3 == 0 # if the number int is divisible by 3
+  if int % 3 == 0 # if the number int is divisible by 3
     "Fizz" # Go fizz
 
   elsif int % 5 == 0 # if the number int is divisible by 3
